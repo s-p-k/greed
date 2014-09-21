c2e815a3d989400656e83a3daac6f13fa8b98ebe
s-p-k <mr.dwts@gmail.com>
2014-09-21 14:40:21 +0300

    add math.scm

---
+++ math.scm
@@ -0,0 +1,18 @@
+;;;; Compute the sum of the geometric progression
+;;;; of 1, 2, 4, 8, ...
+;;;; e.g. geometric-series(5,1) sums the 5 first elements of this
+;;;; geometric progression starting from 1:
+;;;; (+ 1 2 4 8 16) --> 31
+
+(define geometric-series
+  (lambda (bets start)
+    (let ((a (* 2 start)))
+      (geometric-series-iter a bets start))))
+
+(define geometric-series-iter
+  (lambda (a bets start)
+    (let ((result 0))
+      (cond ((eq? bets 1) (- (* 2 (+ result (/ a 2))) start))
+	    (else (begin (set! result (+ result (* 2 a)))
+			 (geometric-series-iter (* 2 a) (- bets 1) start)))))))
+	    
