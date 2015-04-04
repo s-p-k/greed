9201833a11a0bedaaffc2cf464619147930bca4d
s-p-k <mr.dwts@gmail.com>
2015-04-04 06:04:30 +0300

    add greed.scm

---
+++ greed.scm
@@ -0,0 +1,7 @@
+;;;; See LICENSE file for details
+;;;; greed.scm
+
+(load "misc.scm")
+(load "list-utils") ;; always load list-utils before resistors since
+(load "resistors")  ;; uses list-utils
+(format #t "; loaded greed successfully.~%; See LICENSE file for details~%")
