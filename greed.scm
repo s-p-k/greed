dce750e480fc59644ff81d631b1aee951add4b97
s-p-k <mr.dwts@gmail.com>
2015-04-04 06:08:27 +0300

    minor change in format

--- greed.scm
+++ greed.scm
@@ -4,4 +4,4 @@
 (load "misc.scm")
 (load "list-utils") ;; always load list-utils before resistors since
 (load "resistors")  ;; uses list-utils
-(format #t "; loaded greed successfully.~%; See LICENSE file for details~%")
+(format #t "; greed loaded successfully.~%; See LICENSE file for details~%")
