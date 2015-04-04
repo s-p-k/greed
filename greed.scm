edf2ddc20dceb3a94717b3d52fc3bf2ab9a41d9f
s-p-k <mr.dwts@gmail.com>
2015-04-04 06:12:54 +0300

    fix comments, add version

--- greed.scm
+++ greed.scm
@@ -1,7 +1,11 @@
 ;;;; See LICENSE file for details
 ;;;; greed.scm
 
+(define greed-version "0.1")
+
 (load "misc.scm")
-(load "list-utils") ;; always load list-utils before resistors since
-(load "resistors")  ;; uses list-utils
-(format #t "; greed loaded successfully.~%; See LICENSE file for details~%")
+(load "list-utils") ;; always load list-utils before resistors since resistors
+(load "resistors")  ;; use list-utils.
+(format #t
+	"; greed-v~A loaded successfully.~%; See LICENSE file for details~%"
+	greed-version)
