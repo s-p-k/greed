aaa830d16d5d14aabf6c378fbf90b7de59c66033
s-p-k <mr.dwts@gmail.com>
2015-04-04 04:20:00 +0300

    be verbose when user loads misc.scm, add comments

--- misc.scm
+++ misc.scm
@@ -1,8 +1,11 @@
-;;;; Compute the sum of the geometric progression
-;;;; of 1, 2, 4, 8, ...
-;;;; e.g. geometric-series(5,1) sums the 5 first elements of this
-;;;; geometric progression starting from 1:
-;;;; (+ 1 2 4 8 16) --> 31
+;;;; See LICENSE file for details
+;;;; misc.scm is part of the greed project, a collection of Scheme utilities.
+
+;; Compute the sum of the geometric progression
+;; of 1, 2, 4, 8, ...
+;; e.g. geometric-series(5,1) sums the 5 first elements of this
+;; geometric progression starting from 1:
+;; (+ 1 2 4 8 16) --> 31
 
 (define geometric-series
   (lambda (bets start)
@@ -15,4 +18,5 @@
       (cond ((eq? bets 1) (- (* 2 (+ result (/ a 2))) start))
 	    (else (begin (set! result (+ result (* 2 a)))
 			 (geometric-series-iter (* 2 a) (- bets 1) start)))))))
-	    
+
+(format #t "; loaded misc successfully.~%; misc is part of the greed project. See the LICENSE file for details.~%")
